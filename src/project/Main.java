79fd1a79130261bcdb7a6922b3f9bc5a9ecc3f5c
jplee <jplee@kosta.or.kr>
2018-06-16 02:33:50 +0000

    modify comment /* */

--- src/project/Main.java
+++ src/project/Main.java
@@ -17,7 +17,7 @@ public class Main {
     }
     
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        /* TODO Auto-generated method stub */
         //System.out.println("Hello goorm!");
         //System.out.println("Hello Kosta");
         int[] result = new Main().generate();
