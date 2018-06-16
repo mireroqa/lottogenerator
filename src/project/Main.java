30db1f5c666a4fb0ac977da4a5c615687c4f4bab
jplee <jplee@kosta.or.kr>
2018-06-16 01:11:01 +0000

    Add logic

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,14 +1,27 @@
 package project;
 
-public class Main {
+import java.util.Random;
+import java.util.Arrays;
 
-    /**
-     * @param args
-     */
+public class Main {
+    private Random random = new Random(System.nanoTime());
+    
+    public int[] generate(){
+        int[] result = new int[6];
+        
+        for(int i = 0; i < 6; i++)
+        {
+            result[i] = random.nextInt(45);
+        }
+        return result;
+    }
+    
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello goorm!");
-        System.out.println("Hello Kosta");
+        //System.out.println("Hello goorm!");
+        //System.out.println("Hello Kosta");
+        int[] result = new Main().generate();
+        System.out.println(Arrays.toString(result));
     }
 
 }
